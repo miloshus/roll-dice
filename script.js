39937815ca00424db38a3cda7d52586082717dfe
milosvujinovic <vujinovic.milos@gmail.com>
2022-11-06 20:41:19 +0100

    small changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // document.querySelector('.message').textContent = 'Djes djes';
 // document.querySelector('.number').textContent = 21;
 
-const secretNumber = Math.trunc(Math.random()*20)+1;
+let secretNumber = Math.trunc(Math.random()*20)+1;
 let score = 20;
 
 
@@ -49,3 +49,15 @@ document.querySelector('.check').addEventListener('click', function(){
         }
     }
 })
+
+document.querySelector('.again').addEventListener('click', function(){
+    document.querySelector('.message').textContent = 'Start guessing...';
+    score = 20;
+    secretNumber = Math.trunc(Math.random()*20)+1;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+
+   })
