ab1c1cf593179726f7a5e89ecdf336f6631bd8e5
milosvujinovic <vujinovic.milos@gmail.com>
2022-11-07 01:01:56 +0100

    final commit

--- script.js
+++ script.js
@@ -5,7 +5,11 @@
 
 let secretNumber = Math.trunc(Math.random()*20)+1;
 let score = 20;
+let highscore = 0;
 
+const displayMessage = function(message){
+    document.querySelector('.message').textContent = message;
+}
 
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
@@ -14,15 +18,20 @@ document.querySelector('.check').addEventListener('click', function(){
         // When there is no input
 
     if(!guess){
-        document.querySelector('.message').textContent = 'Please enter a number';
+       displayMessage('Please enter a number...');
 
         // When player wins
 
     }else if (guess === secretNumber){
-        document.querySelector('.message').textContent = 'Correct Number!';
+        displayMessage('Correct Number!');
+        // document.querySelector('.message').textContent = 'Correct Number!';
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
         document.querySelector('.number').textContent = secretNumber;
+        if(score > highscore){
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
 
         // When guess is too high 
 
